820b5adcd448ea1bc5af30532c3f481675756334
Robert Di Ninno <robert@onefeather.ca>
2024-03-21 12:22:36 -0700

    update visual name

--- app/routes/art.tsx
+++ app/routes/art.tsx
@@ -6,7 +6,7 @@ export default function Art(){
       <h1>Art</h1>
 
       <Link to="/art/music">Music</Link>
-      <Link to="/art/video">Video</Link>
+      <Link to="/art/visual">Visual</Link>
     </div>
   );
 }

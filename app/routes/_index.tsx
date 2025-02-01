f063f2b2ddbddab4129d04184c0f19c81dfa7dbd
rdininno <robertdininno@gmail.com>
2025-02-01 13:02:33 -0800

    update main page styling

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,17 +13,25 @@ export default function Index() {
     <div className="main-page">
       <header className="header">
         <h1>Robert Di Ninno</h1>
+      </header>
         <div className="description">
           <p>Full stack developer with a passion for the front end.</p>
         </div>
-      </header>
 
-      <div className="menu">
-        <Link className="button button-employment" to ="/employment">Employment</Link>
-        <Link className="button button-art" to ="/art">Art</Link>
+        <div className="btn-container">
+          <Link className="button button-employment" to ="/employment">Experience</Link>
+        </div>
+
+        <div className="btn-container">
+          <Link className="button button-about" to ="/about">About</Link>
+        </div>
+
+      {/* <div className="menu"> */}
+        {/* <Link className="button button-employment" to ="/employment">Employment</Link> */}
+        {/* <Link className="button button-art" to ="/art">Art</Link> */}
         {/* <Link className="button button-portfolio" to ="/portfolio">Portfolio</Link> */}
-        <Link className="button button-about" to ="/about">About</Link>
-      </div>
+        {/* <Link className="button button-about" to ="/about">About</Link> */}
+      {/* </div> */}
     </div>
   );
 }
